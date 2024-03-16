ec718330f553199dddf4b739a585509ae7659479
pc <2528578653@qq.com>
2024-03-16 16:02:28 +0800

    用户删除头像属性

--- src/main/java/cn/wxl475/UserApplication.java
+++ src/main/java/cn/wxl475/UserApplication.java
@@ -1,11 +1,15 @@
 package cn.wxl475;
 
+import cn.wxl475.client.DataClient;
+import cn.wxl475.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("cn.wxl475.mapper")
+@EnableFeignClients(clients = DataClient.class, defaultConfiguration = DefaultFeignConfiguration.class)
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
