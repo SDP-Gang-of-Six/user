3b7925456d8aec54f7d8066655ccbd636dbe51c6
wxl <1715142548@qq.com>
2024-03-24 15:41:08 +0800

    时间类型修改为string，自动填充和数据库已经一并修改

--- src/main/java/cn/wxl475/handler/MyMetaObjectHandler.java
+++ src/main/java/cn/wxl475/handler/MyMetaObjectHandler.java
@@ -1,5 +1,6 @@
 package cn.wxl475.handler;
 
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
@@ -13,17 +14,17 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     //插入时的填充策略
     @Override
     public void insertFill(MetaObject metaObject) {
-        log.info("start insert fill ....");
-        this.strictInsertFill(metaObject,"createTime", LocalDateTime.class,LocalDateTime.now());// 起始版本 3.3.0(推荐使用)
-        log.info("start update fill ....");
-        this.strictInsertFill(metaObject,"updateTime", LocalDateTime.class,LocalDateTime.now());// 起始版本 3.3.0(推荐使用)
-
+        log.info("插入：填充createTime");
+        this.strictInsertFill(metaObject,"createTime", String.class, DateUtil.now());// 起始版本 3.3.0(推荐使用)
+        log.info("插入：填充updateTime");
+        this.strictInsertFill(metaObject, "updateTime", String.class, DateUtil.now()); // 起始版本 3.3.0(推荐)
     }
 
     //更新时的填充策略
     @Override
     public void updateFill(MetaObject metaObject) {
-        log.info("start update fill ....");
-        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
+        log.info("更新：填充updateTime");
+        this.strictUpdateFill(metaObject, "updateTime", String.class, DateUtil.now()); // 起始版本 3.3.0(推荐)
     }
 }
+
